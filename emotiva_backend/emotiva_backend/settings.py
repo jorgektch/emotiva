20486bb17a94f3254e7baacca247f59bd1494142
Jorge Ktch <jorgektch@gmail.com>
2025-07-28 05:49:54 -0500

    Add file

--- emotiva_backend/emotiva_backend/settings.py
+++ emotiva_backend/emotiva_backend/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a+j5xw!g=$4p4u6(8ff$16prr+2-u8q+@6l#3c!vasg^46l0tv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
